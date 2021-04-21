e994f8075643c61e9d22c56694dcce893283350e
iker_martin <al343083@uji.es>
2021-04-21 11:22:56 +0200

    Se han movido unos ficheros

--- Codes/compila.sh
+++ Codes/compila.sh
@@ -1,3 +1,3 @@
 module load /home/martini/MODULES/modulefiles/mpich3.4
 
-mpicc -Wall Main.c ../IOcodes/read_ini.c ../IOcodes/ini.c ../malleability/ProcessDist.c ../malleability/CommDist.c -pthread -lslurm
+mpicc -Wall Main/Main.c IOcodes/read_ini.c IOcodes/ini.c malleability/ProcessDist.c malleability/CommDist.c -pthread -lslurm
