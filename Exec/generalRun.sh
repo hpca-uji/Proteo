8ad61042a6c98326d4f2abcd4c7a0cde9a7e7a20
iker_martin <al343083@uji.es>
2022-11-24 21:04:49 +0100

    Added Exec scripts for performing Extrae Runs

---
+++ Exec/generalRun.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#This script should only be called by others scripts, do not call it directly
+
+#SBATCH --exclude=c02,c01,c00
+#SBATCH -p P1
+codeDir="/Codes/build"
+
+echo "START TEST"
+
+#$1 == baseDir
+#$2 == configFile
+#$3 == use_extrae
+#$4 == outFileIndex
+
+echo $@
+if [ $# -lt 3 ]
+then
+  echo "Internal ERROR generalRun.sh - Not enough arguments were given"
+  exit -1
+fi
+
+dir=$1
+configFile=$2
+use_extrae=$3
+outFileIndex=$4
+
+aux=$(grep "\[resize0\]" -n $configFile | cut -d ":" -f1)
+read -r ini fin <<<$(echo $aux)
+diff=$(( fin - ini ))
+numP=$(head -$fin $configFile | tail -$diff | cut -d ';' -f1 | grep Procs | cut -d '=' -f2)
+
+echo "Nodes=$SLURM_JOB_NODELIST"
+if [ $use_extrae -ne 1 ]
+then
+  mpirun -np $numP $dir$codeDir/a.out $configFile $outFileIndex $SLURM_JOB_NODELIST $SLURM_JOB_NUM_NODES 
+else
+  # EXTRAE Configuration
+  export EXTRAE_CONFIG_FILE=./extrae.xml
+  export LD_PRELOAD=${EXTRAE_HOME}/lib/libmpitrace.so
+  srun -n$numP --mpi=pmi2 $dir$codeDir/a.out $configFile $outFileIndex $SLURM_JOB_NODELIST $SLURM_JOB_NUM_NODES
+fi
+
+
+echo "END TEST"
+sed -i 's/application called MPI_Abort(MPI_COMM_WORLD, -100) - process/shrink cleaning/g' slurm-$SLURM_JOB_ID.out
---
+++ Exec/runTrace.sh
@@ -0,0 +1,53 @@
+dir="/home/martini/malleability_benchmark"
+#procs=(2 10 20 40 80 120 160)
+procs=(2)
+cores=20
+
+
+#====== Do not modify these values =======
+
+codeDir="/Codes/build"
+execDir="/Exec"
+ResultsDir="/Results"
+use_extrae=1
+
+#Extrae config
+qty=1
+outFileIndex=$2
+
+if [ $# -ge 1 ]
+then
+  qty=$1
+fi
+
+for proc in "${procs[@]}"
+do
+  echo "------------------------------------------run np=$proc"
+
+  node_qty=$(($proc / $cores))
+  if [ $node_qty -eq 0 ]
+  then
+    node_qty=1
+  fi
+
+  config_file="test$proc"".ini"
+  upper_dir="Proc$proc"
+  mkdir $upper_dir
+  cd $upper_dir
+  for ((i=0; i<qty; i++))
+  do
+    # Move data to new directory
+    lower_dir="Run$i"
+    mkdir $lower_dir
+    cd $lower_dir
+    cp $dir$execDir/extrae.xml .
+    cp ../../$config_file .
+
+    #pwd
+    sbatch -p P1 -N $node_qty $dir$execDir/generalRun.sh $dir $config_file $use_extrae $outFileIndex
+    cd ..
+  done
+  cd ..
+
+done
+echo "End"
